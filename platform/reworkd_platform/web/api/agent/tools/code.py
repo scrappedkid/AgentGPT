61394b6961016877a2748fe922e726ea9c0993fd
scrappedkid <51921165+scrappedkid@users.noreply.github.com>
2023-11-06 13:13:50 -0500

    Update code.py

--- platform/reworkd_platform/web/api/agent/tools/code.py
+++ platform/reworkd_platform/web/api/agent/tools/code.py
@@ -1,39 +1,60 @@
+
 from typing import Any
+from datetime import datetime
 
-from fastapi.responses import StreamingResponse as FastAPIStreamingResponse
-from lanarky.responses import StreamingResponse
+from fastapi.responses import StreamingResponse
+from lanarky.responses import StreamingResponse as LanarkyStreamingResponse
 from langchain import LLMChain
-
 from reworkd_platform.web.api.agent.tools.tool import Tool
 
 
 class Code(Tool):
     description = "Should only be used to write code, refactor code, fix code bugs, and explain programming concepts."
-    public_description = "Write and review code."
+    public_description = "Write and review code"
 
     async def call(
         self, goal: str, task: str, input_str: str, *args: Any, **kwargs: Any
-    ) -> FastAPIStreamingResponse:
+    ) -> StreamingResponse:
         from reworkd_platform.web.api.agent.prompts import code_prompt
 
         chain = LLMChain(llm=self.model, prompt=code_prompt)
 
-        # generated code
-        code = # generated code
-        self.write_to_shared_folder(f'{goal}_{task}.py', code)
-        tags = self.generate_tags(code)
-        self.write_to_shared_folder(f'{goal}_{task}_tags.txt', ', '.join(tags))
+        code_stream = await self.generate_code_stream(chain)
 
-        return StreamingResponse.from_chain(
-            chain,
-            {"goal": goal, "language": self.language, "task": task},
+        # Parse the generated code from the streaming response
+        code = self.parse_code_from_stream(code_stream)
+
+        filename = f"{goal}_{task}_{datetime.now().strftime('%Y%m%d%H%M%S')}.py"
+        self.write_to_shared_folder(filename, code)
+
+        # Generate tags
+        tags = self.generate_tags(code)
+        tags_filename = f"{goal}_{task}_{datetime.now().strftime('%Y%m%d%H%M%S')}_tags.txt"
+        self.write_to_shared_folder(tags_filename, ", ".join(tags))
+
+        return StreamingResponse(
+            code_stream,
             media_type="text/event-stream",
         )
 
+    async def generate_code_stream(self, chain: LLMChain) -> bytes:
+        # Generate code using LLMChain and return as a byte stream
+        # Example implementation:
+        code = await chain.generate_text(prompt="Generate code")
+        return code.encode("utf-8")
+
+    def parse_code_from_stream(self, code_stream: bytes) -> str:
+        # Parse the code from the byte stream
+        code = code_stream.decode("utf-8")
+        # Implement code parsing logic based on the specific format in the streaming response
+        # Example implementation:
+        code = code.strip().replace("```python", "").replace("```", "").strip()
+        return code
+
     def write_to_shared_folder(self, filename: str, content: str) -> None:
-        with open(f'shared_folder/{filename}', 'w') as f:
+        with open(f"shared_folder/{filename}", "w") as f:
             f.write(content)
 
     def generate_tags(self, code: str) -> list:
-        # Implement your tag generation logic here
+        # Implement tag generation logic based on the requirements or specifications
         return []
