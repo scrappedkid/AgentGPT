f6741efd8733e5b59f64bac9a547578dc0599c9d
scrappedkid <51921165+scrappedkid@users.noreply.github.com>
2023-11-26 08:05:14 +0000

    fix

--- platform/reworkd_platform/web/api/agent/tools/file_git_code_tool.py
+++ platform/reworkd_platform/web/api/agent/tools/file_git_code_tool.py
@@ -1,37 +1,37 @@
-   import os
-   import shutil
-   import subprocess
-   from git import Repo
-   from reworkd_platform.web.api.agent.tools.tool import Tool
+import os
+import shutil
+import subprocess
+from git import Repo
+from reworkd_platform.web.api.agent.tools.tool import Tool
 
-   class FileGitCodeTool(Tool):
-       description = "Handle file operations, Git functionalities, and code execution."
-       public_description = "Read and write Python, Markdown, and Text files, handle Git operations, and execute code."
+class FileGitCodeTool(Tool):
+    description = "Handle file operations, Git functionalities, and code execution."
+    public_description = "Read and write Python, Markdown, and Text files, handle Git operations, and execute code."
 
-       def read_file(self, file_path):
-           with open(file_path, 'r') as file:
-               return file.read()
+    def read_file(self, file_path):
+        with open(file_path, 'r') as file:
+            return file.read()
 
-       def write_file(self, file_path, content):
-           with open(file_path, 'w', encoding='utf-8') as file:
-               file.write(content)
+    def write_file(self, file_path, content):
+        with open(file_path, 'w', encoding='utf-8') as file:
+            file.write(content)
 
-       def clone_repo(self, repo_url, dir_path):
-           Repo.clone_from(repo_url, dir_path)
+    def clone_repo(self, repo_url, dir_path):
+        Repo.clone_from(repo_url, dir_path)
 
-       def pull_repo(self, dir_path):
-           repo = Repo(dir_path)
-           origin = repo.remotes.origin
-           origin.pull()
+    def pull_repo(self, dir_path):
+        repo = Repo(dir_path)
+        origin = repo.remotes.origin
+        origin.pull()
 
-       def push_repo(self, dir_path):
-           repo = Repo(dir_path)
-           origin = repo.remotes.origin
-           origin.push()
+    def push_repo(self, dir_path):
+        repo = Repo(dir_path)
+        origin = repo.remotes.origin
+        origin.push()
 
-       def execute_code(self, file_path):
-           subprocess.run(["python", file_path])
+    def execute_code(self, file_path):
+        subprocess.run(["python", file_path])
 
-       async def call(self, goal, task, input_str, *args, **kwargs):
-           # TODO: Implement the logic to perform the appropriate file operation, Git functionality, or code execution based on the task.
-           pass
+    async def call(self, goal, task, input_str, *args, **kwargs):
+        # TODO: Implement the logic to perform the appropriate file operation, Git functionality, or code execution based on the task.
+        pass
